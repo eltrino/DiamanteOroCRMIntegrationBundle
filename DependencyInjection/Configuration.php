5caafbc8010d95033b7f122e8a7e1e3aa83a1c57
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-08-03 17:09:25 +0300

    0~DIAM-999 Create configuration option1~

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Diamante\OroCRMIntegrationBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
@@ -20,9 +21,15 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('diamante_oro_crm_integration');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        SettingsBuilder::append(
+            $rootNode,
+            array(
+                'create_contact' => array(
+                    'value' => '',
+                    'type' => 'scalar'
+                )
+            )
+        );
 
         return $treeBuilder;
     }
--- DependencyInjection/DiamanteOroCRMIntegrationExtension.php
+++ DependencyInjection/DiamanteOroCRMIntegrationExtension.php
@@ -22,6 +22,8 @@ class DiamanteOroCRMIntegrationExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
